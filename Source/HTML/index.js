bcaacf3490f960155b2509af14d92aceacadacd7
Langning Chen <chenlangning2009@outlook.com>
2025-01-20 09:47:05 +0800

    Fix admin refresh gone

--- Source/HTML/index.js
+++ Source/HTML/index.js
@@ -334,9 +334,10 @@ const CreateHorizontalLine = () => {
 
 (() => {
     if (localStorage.getItem("IsAdmin") !== null) {
+        debugger;
         const AddonStyle = document.getElementById("AddonStyle");
         AddonStyle.innerHTML = ".NotLoginOnly { display: none; }";
-        if (localStorage.getItem("IsAdmin") != true) {
+        if (localStorage.getItem("IsAdmin") !== "true") {
             AddonStyle.innerHTML += ".AdminOnly { display: none; }";
         }
     }
